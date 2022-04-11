5110e25e4ea75357517893621cdc9b2c84f1385f
cscald <319374873@qq.com>
2022-04-11 23:48:55 +0800

    初始化项目

--- vue.config.js
+++ vue.config.js
@@ -2,3 +2,16 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true
 })
+module.exports = {
+  configureWebpack: {
+    resolve: {
+      alias: {
+        // '@' 是 './src' 的别名，vue-cli 默认配置
+        'assets': '@/assets',
+        'components': '@/components',
+        'network': '@/network',
+        'views': '@/views'
+      }
+    }
+  }
+}
